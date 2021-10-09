e4b628287a88585cb5807ba3d4c4e52299bdb191
Encgeorge <enciugeorge99@gmail.com>
2021-10-09 23:04:18 +0300

    deploy my react app to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import profile from './img/eu.jpg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> Hello to the LoMb Platform</h1>
+    <img src={profile} alt="profile image"/>  
     </div>
   );
 }
